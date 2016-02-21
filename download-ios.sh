9b55dcacf24a0e23607b7488617d61b93e5c7721
Francis Chong <francis@ignition.hk>
2016-02-21 09:22:49 +0800

    add download static library script

---
+++ download-ios.sh
@@ -0,0 +1,7 @@
+VERSION=61cf580b182c2f91a9853a2bf0fa99221d3fb905
+URL="https://github.com/chrisballinger/ffmpeg-ios-static-libs/archive/${VERSION}.zip"
+UNZIP_DIR="ffmpeg-ios-static-libs-${VERSION}"
+
+wget $URL -O ffmpef-ios-static-libs.zip
+unzip ffmpef-ios-static-libs.zip
+mv $UNZIP_DIR "ffmpeg-ios-static"
