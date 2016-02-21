6335ed1249466ce69654657c72364203878273ba
Francis Chong <francis@ignition.hk>
2016-02-21 11:11:53 +0800

    add script to download static binaries

---
+++ download-ios.sh
@@ -0,0 +1,8 @@
+VERSION=k0.4.5.1
+URL="https://www.dropbox.com/s/g2tem4ltmj6d46n/build-${VERSION}.tgz?dl=1"
+DOWNLOADED_FILE="build-${VERSION}.tgz"
+UNZIP_DIR="ffmpeg-ios-static-libs-${VERSION}"
+
+mkdir vendor
+cd vendor
+wget $URL -O $DOWNLOADED_FILE && tar vxfz $DOWNLOADED_FILE
